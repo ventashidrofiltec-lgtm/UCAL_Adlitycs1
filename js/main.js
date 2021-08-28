5e4dda5326a49ec174be9b7b5e1ecd86b1902566
KuroDev <aurelio.dc.89@gmail.com>
2021-08-28 17:42:41 -0500

    Âsocial icons added | texts added | colors applied | footer modified | back-to-top button added

--- js/main.js
+++ js/main.js
@@ -132,45 +132,7 @@ $(document).ready(function() {
   });
 });
 
-/*BACK TO TOP BUTTON*/
-$(document).ready(function() {
-  $('.top').click(function() {
-    $('body, html').animate(
-      {
-        scrollTop: '0px'
-      },
-      1000
-    );
-  });
 
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 0) {
-      $('.top').slideDown(0);
-    } else {
-      $('.top').slideUp(0);
-    }
-  });
-});
-
-/*BACK TO TOP BUTTON mobile*/
-$(document).ready(function() {
-  $('.top2').click(function() {
-    $('body').animate(
-      {
-        scrollTop: '0px'
-      },
-      1000
-    );
-  });
-
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 0) {
-      $('.top2').slideDown(0);
-    } else {
-      $('.top2').slideUp(0);
-    }
-  });
-});
 
 /*VALIDA EL NÚMERO DE TELÉFONO*/
 function justNumbers(e) {
--- mail.php
+++ mail.php
@@ -3,7 +3,6 @@ if(isset($_POST['submit'])){
 $destino="aurelio.dc.89@gmail.com";
 $nombre=$_POST["first_name"];
 $correo=$_POST["email"];
-$telf=$_POST["phone"];
 $asunto=$_POST["asunto"];
 $mesage=$_POST["message"];
 
@@ -16,7 +15,7 @@ if ($_POST['stopspam'] != ""){
  
 }
 
-$contenido="Nombre: " . $nombre . "\nCorreo: " . $correo . "\nTel�fono: " . $telf . "\nAsunto: " . $asunto . "\nMensaje: "  . $mesage;
+$contenido="Nombre: " . $nombre . "\nCorreo: " . $correo . "\nAsunto: " . $asunto . "\nMensaje: "  . $mesage;
 mail($destino, "Contacto", $contenido);
  
 header("Location:thanks.html");
